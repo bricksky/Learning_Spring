93ba1480060e9ff32998f41e3596b4573ae7fdf4
bricksky <skydh1214@naver.com>
2024-09-18 23:55:18 +0900

    회원 리포지토리 테스트 케이스 작성

---
+++ hello-spring/src/test/java/bricksky/hello_spring/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,59 @@
+package bricksky.hello_spring.repository;
+
+import bricksky.hello_spring.domain.Member;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class MemoryMemberRepositoryTest {
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    // test 끝날 때 마다 지워주는 코드
+    @AfterEach
+    public void afterEach(){
+        repository.clearStore();
+    }
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("BrickSky");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        // Assertions.assertEquals(member, result);
+        Assertions.assertThat(member).isEqualTo(result);
+    }
+
+    @Test
+    public void findByName(){
+        Member member1 = new Member();
+        member1.setName("BrickSky1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("BrickSky2");
+        repository.save(member2);
+
+        Member result = repository.findByname("BrickSky1").get();
+        Assertions.assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll(){
+        Member member1 = new Member();
+        member1.setName("BrickSky1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("BrickSky2");
+        repository.save(member2);
+        List<Member> result = repository.findAll();
+
+        Assertions.assertThat(result.size()).isEqualTo(2);
+    }
+}
+
