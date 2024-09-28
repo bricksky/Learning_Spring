e374efb5db7e8f0ef8fcafc717648dd312e7fb40
bricksky <skydh1214@naver.com>
2024-09-28 18:27:36 +0900

    회원 리포지토리 테스트 케이스 작성

--- hello-spring/src/test/java/bricksky/hello_spring/repository/MemoryMemberRepositoryTest.java
+++ hello-spring/src/test/java/bricksky/hello_spring/repository/MemoryMemberRepositoryTest.java
@@ -6,54 +6,56 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
+import java.util.Optional;
 
-public class MemoryMemberRepositoryTest {
+import static org.assertj.core.api.Assertions.*;
+
+class MemoryMemberRepositoryTest {
+
     MemoryMemberRepository repository = new MemoryMemberRepository();
 
-    // test 끝날 때 마다 지워주는 코드
-    @AfterEach
-    public void afterEach(){
-        repository.clearStore();
-    }
+     @AfterEach
+     public void afterEach(){
+         repository.clearStore();
+     }
 
-    @Test
+     @Test
     public void save(){
-        Member member = new Member();
-        member.setName("BrickSky");
+         Member member = new Member();
+         member.setName("spring");
 
-        repository.save(member);
+         repository.save(member);
 
-        Member result = repository.findById(member.getId()).get();
-        // Assertions.assertEquals(member, result);
-        Assertions.assertThat(member).isEqualTo(result);
-    }
+         Member result = repository.findById(member.getId()).get();
+         assertThat(member).isEqualTo(result);
+     }
 
-    @Test
-    public void findByName(){
-        Member member1 = new Member();
-        member1.setName("BrickSky1");
-        repository.save(member1);
+     @Test
+    public void findByname(){
+         Member member1 = new Member();
+         member1.setName("spring1");
+         repository.save(member1);
 
-        Member member2 = new Member();
-        member2.setName("BrickSky2");
-        repository.save(member2);
+         Member member2 = new Member();
+         member2.setName("spring2");
+         repository.save(member2);
 
-        Member result = repository.findByname("BrickSky1").get();
-        Assertions.assertThat(result).isEqualTo(member1);
-    }
+         Member result = repository.findByname("spring1").get();
 
-    @Test
+         assertThat(result).isEqualTo(member1);
+     }
+
+     @Test
     public void findAll(){
-        Member member1 = new Member();
-        member1.setName("BrickSky1");
-        repository.save(member1);
+         Member member1 = new Member();
+         member1.setName("spring1");
+         repository.save(member1);
 
-        Member member2 = new Member();
-        member2.setName("BrickSky2");
-        repository.save(member2);
-        List<Member> result = repository.findAll();
+         Member member2 = new Member();
+         member2.setName("spring2");
+         repository.save(member2);
 
-        Assertions.assertThat(result.size()).isEqualTo(2);
-    }
+         List<Member> result = repository.findAll();
+         assertThat(result.size()).isEqualTo(2);
+     }
 }
-
