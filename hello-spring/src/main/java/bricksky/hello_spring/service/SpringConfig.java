b07de62aa01cabafc43db52a81256d51277187ec
bricksky <skydh1214@naver.com>
2024-10-04 23:43:00 +0900

    JPA

--- hello-spring/src/main/java/bricksky/hello_spring/service/SpringConfig.java
+++ hello-spring/src/main/java/bricksky/hello_spring/service/SpringConfig.java
@@ -1,8 +1,11 @@
 package bricksky.hello_spring.service;
 
 import bricksky.hello_spring.repository.JdbcMemberRepository;
+import bricksky.hello_spring.repository.JpaMemberRepository;
 import bricksky.hello_spring.repository.MemberRepository;
 import bricksky.hello_spring.repository.MemoryMemberRepository;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,12 +13,18 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+   private EntityManager em;
+//    private DataSource dataSource;
+//
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
 
-    private DataSource dataSource;
-
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    @Autowired
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
+
     @Bean
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -25,6 +34,7 @@ public class SpringConfig {
     public MemberRepository memberRepository(){
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
